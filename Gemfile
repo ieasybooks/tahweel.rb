5fc55ec2387c31bb52cb87ff9250e0bb8fb20a46
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-11-28 15:04:31 +0300

    Install rubocop plugins

--- Gemfile
+++ Gemfile
@@ -11,3 +11,6 @@ gem "rake", "~> 13.0"
 gem "rspec", "~> 3.13"
 
 gem "rubocop", "~> 1.81"
+gem "rubocop-performance", "~> 1.26", ">= 1.26.1"
+gem "rubocop-rake", "~> 0.7.1"
+gem "rubocop-rspec", "~> 3.8"
--- spec/tahweel_spec.rb
+++ spec/tahweel_spec.rb
@@ -2,10 +2,6 @@
 
 RSpec.describe Tahweel do
   it "has a version number" do
-    expect(Tahweel::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(Tahweel::VERSION).not_to be_nil
   end
 end
