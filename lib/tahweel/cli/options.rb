ca2672f9710fb8cae7effef4afe090e946a90d93
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-11-30 22:18:41 +0300

    Add concurrency flags to options

--- lib/tahweel/cli/options.rb
+++ lib/tahweel/cli/options.rb
@@ -52,37 +52,37 @@ module Tahweel
           "-e", "--extensions EXTENSIONS", Array,
           "Comma-separated list of file extensions to process " \
           "(default: #{Tahweel::CLI::FileCollector::SUPPORTED_EXTENSIONS.join(", ")})"
-        ) do |e|
-          options[:extensions] = e
+        ) do |value|
+          options[:extensions] = value
         end
 
-        opts.on("--dpi DPI", POSITIVE_INTEGER, "DPI for PDF to Image conversion (default: 150)") do |d|
-          options[:dpi] = d
+        opts.on("--dpi DPI", POSITIVE_INTEGER, "DPI for PDF to Image conversion (default: 150)") do |value|
+          options[:dpi] = value
         end
 
         opts.on(
           "-p", "--processor PROCESSOR", Tahweel::Ocr::AVAILABLE_PROCESSORS,
           "OCR processor to use (default: google_drive). Available: #{Tahweel::Ocr::AVAILABLE_PROCESSORS.join(", ")}"
-        ) do |p|
-          options[:processor] = p
+        ) do |value|
+          options[:processor] = value
         end
 
-        opts.on("--page-concurrency N", POSITIVE_INTEGER, "Max concurrent OCR operations (default: 12)") do |n|
-          options[:page_concurrency] = n
+        opts.on("-P", "--page-concurrency PAGE_CONCURRENCY", POSITIVE_INTEGER, "Max concurrent OCR operations (default: 12)") do |value|
+          options[:page_concurrency] = value
         end
 
         opts.on(
-          "--file-concurrency N", POSITIVE_INTEGER,
+          "-F", "--file-concurrency FILE_CONCURRENCY", POSITIVE_INTEGER,
           "Max number of files to process in parallel (default: 1)"
-        ) do |n|
-          options[:file_concurrency] = n
+        ) do |value|
+          options[:file_concurrency] = value
         end
 
         opts.on(
           "-f", "--formats FORMATS", Array,
           "Output formats (comma-separated, default: txt). Available: #{Tahweel::Writer::AVAILABLE_FORMATS.join(", ")}"
-        ) do |formats|
-          options[:formats] = formats.map(&:to_sym)
+        ) do |value|
+          options[:formats] = value.map(&:to_sym)
 
           invalid_formats = options[:formats] - Tahweel::Writer::AVAILABLE_FORMATS
           abort "Error: invalid format(s): #{invalid_formats.join(", ")}" if invalid_formats.any?
@@ -91,12 +91,12 @@ module Tahweel
         opts.on(
           "--page-separator SEPARATOR", String,
           "Separator between pages in TXT output (default: #{Tahweel::Writers::Txt::PAGE_SEPARATOR.gsub("\n", "\\n")})"
-        ) do |s|
-          options[:page_separator] = s.gsub("\\n", "\n")
+        ) do |value|
+          options[:page_separator] = value.gsub("\\n", "\n")
         end
 
-        opts.on("-o", "--output DIR", String, "Output directory (default: current directory)") do |o|
-          options[:output] = o
+        opts.on("-o", "--output DIR", String, "Output directory (default: current directory)") do |value|
+          options[:output] = value
         end
       end
 
